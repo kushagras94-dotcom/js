8909a22536fea0d9a1d8cb9a6b78582b338747ab
kushagras94-dotcom <kushagras94@gmail.com>
2026-05-26 18:10:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer ver
+// alert(3+3) we are using nodejs not browser
+
+//number, bigint
+//string boolean
+//null
+//undefined
+//symbol
+
+//object
+console.log(typeof("kush"));
+ 
+console.log(typeof undefined);//undefiend
+console.log(typeof null);//object
+
+
