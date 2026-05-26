0c3bf6ee7cf7dcc9c40c3f328f5bc4a1fb9eee6a
kushagras94-dotcom <kushagras94@gmail.com>
2026-05-26 18:10:22 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId= 144553
+let accountEmail="kush@g.com"
+var accountPassword="jaipur"
+let name;
+/*Never use var please*/
+console.table([accountEmail,accountId,accountPassword, name])
