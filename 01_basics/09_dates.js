3c2370b41d3f592ee3f8b13cdf38c6f98baf5dee
kushagras94-dotcom <kushagras94@gmail.com>
2026-05-28 07:21:11 +0000

    dates

---
+++ 01_basics/09_dates.js
@@ -0,0 +1,14 @@
+//date is object
+let myDate= new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+let myCreatedDate=new Date(2026, 4, 28, 5, 3)
+console.log(myCreatedDate.toLocaleString());
+ console.log(myCreatedDate.getTime());
+ let myTime= Date.now()
+
+//getDay getMonth+1  
+newDate.toLocaleString('default', {
+    weekday:"long",
+}) 
