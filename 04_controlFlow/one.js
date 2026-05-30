d39d40e2419c6bb25e40401fcb3be691e06b4fc7
kushagras94-dotcom <kushagras94@gmail.com>
2026-05-30 18:23:37 +0000

    conditional

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,16 @@
+//if
+if(true){//>,<,>=,<=,==,===,!=,!==
+}
+if(true) console.log("rtdt"), console.log("hdkedj"); //can be odne but not good practice
+
+const month=3
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+
+    default:
+        break;
+}
+
