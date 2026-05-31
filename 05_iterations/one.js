679f723d6acadf604e0a3e88ce7a58f961988eb9
kushagras94-dotcom <kushagras94@gmail.com>
2026-05-31 12:55:55 +0000

    just basic iterators

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -3,3 +3,27 @@
 //while
 //do while
 
+// for of
+["","",""]
+[{},{},{}]
+
+const arr = [1,2,3,4,5]
+for(const i of arr){
+    console.log(i);
+}
+const time="Five Fifty"
+for(const i of time){
+    console.log(`Each char is ${i}`);
+}
+
+//Maps
+const map=new Map()
+map.set('IN', "India")
+map.set('UK', "United Kingdom")
+map.set('IN', "India")
+console.log(map);
+
+for(const [key, value] of map){
+    console.log(key, ':-', value);
+}
+
