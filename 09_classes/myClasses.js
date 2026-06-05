5c46926295c480a467f470035230b0cc75dede7f
kushagras94-dotcom <kushagras94@gmail.com>
2026-06-05 20:17:21 +0000

    again

---
+++ 09_classes/myClasses.js
@@ -0,0 +1,41 @@
+//ESG
+
+class User {
+    constructor(username, email, password){
+        this.username =username
+        this.email=email
+        this.password=password
+    }
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+const k=new User("Kush" , "kush@yahoo", "123")
+console.log(k.encryptPassword());
+
+console.log(k.changeUsername());
+
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
